7e03b975e8ad762053f14ff1b91dec4938c30478
Derek Lee <derekclee323@g.ucla.edu>
2022-03-10 19:33:53 -0800

    Remove logging

--- mouse-picker.js
+++ mouse-picker.js
@@ -33,7 +33,6 @@ export class MousePicker extends defs.Movement_Controls {
         // Convert from left to right handed coordinate
         this.ray = Mat4.scale(1, 1, -1).times(this.ray);
         this.ray = vec(this.ray[0], this.ray[1], this.ray[2]).normalized();
-        console.log(this.ray);
         return this.ray;
     }
 
